36c8c0d926ea93728a15e2eb68ee4d8396006cbb
Sarwar Jahan <shohidmax@gmail.com>
2024-03-21 03:16:30 +0600

    long2

--- arduino_soket_io/index.js
+++ arduino_soket_io/index.js
@@ -1,18 +1,33 @@
 const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const { Board, Led } = require('johnny-five');
+
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const server = http.createServer(app);
+const io = socketIo(server);
 
-app.use(express.static('public')); // To serve your web client files
+app.use(express.static(__dirname + '/public'));
 
-io.on('/connection', (socket) => {
-  console.log('Client connected');
-
-  socket.on('togglePC', () => {
-	io.emit('pcPower'); // Broadcast to all clients to trigger NodeMCU
-  });
+server.listen(3000, () => {
+  console.log('Server running on port 3000');
 });
 
-http.listen(3009, () => {
-  console.log('Server listening on port 3009');
+const board = new Board();
+
+board.on('ready', () => {
+  const led = new Led(2); // Assuming the LED is connected to pin 2 of the NodeMCU
+
+  io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    socket.on('toggle', () => {
+      led.toggle();
+      io.emit('status', led.isOn ? 'on' : 'off');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Client disconnected');
+    });
+  });
 });
